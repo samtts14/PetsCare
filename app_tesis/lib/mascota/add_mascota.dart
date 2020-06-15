2504a8b05c7da80a9b47a4d0fa6be37a1ee0b31c
Yohn28 <59832006+Yohn28@users.noreply.github.com>
2020-06-15 16:55:39 -0400

    funciona uno de los dropdown button

--- app_tesis/lib/mascota/add_mascota.dart
+++ app_tesis/lib/mascota/add_mascota.dart
@@ -11,17 +11,49 @@ class AddMascota extends StatefulWidget {
 
 class _AddMascotaState extends State<AddMascota> {
   
+  final GlobalKey<FormState> _formkeyValue = new GlobalKey<FormState>();
   DateTime _dueDate = new DateTime.now();
   String _dateText = '';
 
   // final FirebaseAuth _firebaseAuth;
 
+  List<String> _tipoDeMascota = <String>[
+    'Perro',
+    'Gato',
+    'Ave',
+  ];
+  List<String> _razaDeMascota = <String>[
+    'Chow-Chow',
+    'Chihuahua',
+    'Husky',
+  ];
+
   String name = '';
-  String especie = '';
-  String raza = '';
+  String especie = 'Perro';
+  String raza = 'Chow-Chow';
   String edad = '';
   String sexo = '';
   String owner = '';
+
+  Future<Null> _razasPorMascota(String especie) async{
+    switch( especie){
+      case 'Perro': { _razaDeMascota = <String>[
+                      'Chow-Chow',
+                      'Chihuahua',
+                      'Husky',];}
+      break;
+      case 'Gato': {_razaDeMascota = <String>[
+                      'British Shorthair',
+                      'Coon De Maine',
+                      'Persas Doll',];}
+      break;
+      case 'Ave': {_razaDeMascota = <String>[
+                      'Cacatua',
+                      'Loro',
+                      'Perico',];}
+      break;
+      }
+    }
 
   Future<Null> _selectDueDate(BuildContext context) async{
     final picked = await showDatePicker(
@@ -64,128 +96,119 @@ class _AddMascotaState extends State<AddMascota> {
 
   @override
   Widget build(BuildContext context) {
-    return new Material(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            width:double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.purple
-            ),
-            child: Column(
-              children: <Widget>[
-              Text('Mascota', style: new TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                letterSpacing: 2.0,
-                fontFamily: "Pacifico" ),
-              ),
-            ],
-          )
-        ),
-        new Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            onChanged: (String str){
-              setState(() {
-                name = str;
-              });
-            },
-            decoration: new InputDecoration(
-              icon: Icon(Icons.dashboard),
-              hintText: 'Nombre de Mascota',
-              border: InputBorder.none
-            )
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: Colors.white
           ),
+          onPressed: () {},
         ),
-        new Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            onChanged: (String str){
-              setState(() {
-                especie = str;
-              });
-            },
-            decoration: new InputDecoration(
-              icon: Icon(Icons.dashboard),
-              hintText: 'Especie',
-              border: InputBorder.none
-            )
-          ),
-        ),  
-        new Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            onChanged: (String str){
-              setState(() {
-                raza = str;
-              });
-            },
-            decoration: new InputDecoration(
-              icon: Icon(Icons.dashboard),
-              hintText: 'Raza',
-              border: InputBorder.none
-            )
-          ),
-        ),
-        new Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: new Row(
-            children: <Widget>[
-              new Icon(Icons.date_range),
-              new Expanded(child: Text('Fecha de nacimiento o Adquisicion', style: new TextStyle(fontSize: 16.0)),),
-              new FlatButton(
-                onPressed: () => _selectDueDate(context) ,
-                child: Text(_dateText, style: new TextStyle(fontSize: 16.0))
-              )
-            ],
-          )
+        title: Container(
+          alignment: Alignment.centerLeft,
+          child: Text("Datos de Mascota", style: TextStyle(color: Colors.white),),
         ),
         
-        
-        new Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            onChanged: (String str){
-              setState(() {
-                sexo = str;
-              });
-            },
-            decoration: new InputDecoration(
-              icon: Icon(Icons.dashboard),
-              hintText: 'Sexo',
-              border: InputBorder.none
-            )
-          ),
-        ),
-        new Padding(
-          padding: const EdgeInsets.only(top: 100.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.check, size: 40.0),
-                onPressed: () async{
-                  final currentUser = await FirebaseAuth.instance.currentUser();
-                  owner = currentUser.email.toString();
-                  _addData();
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.close, size: 40.0),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-              )
-              
-            ],
-          )
-          
-          )
-        ]
-      )
+      ),
       
+      body: Form(
+        key: _formkeyValue,
+        autovalidate: true,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.create,
+                  color: Colors.teal
+                ),
+                hintText: 'Nombre de Mascota',
+                labelText: 'Nombre'
+              ),
+
+            ),
+
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.pets,
+                size: 25.0,
+                color: Colors.brown,),
+                
+                DropdownButton<String>(
+                  value: especie,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      especie = newValue;
+                      _razasPorMascota(especie);
+                    });
+                  },
+                  
+                  items: <String>['Perro','Ave', 'Gato', ]
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )
+              ],
+            ),
+            
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.pets,
+                size: 25.0,
+                color: Colors.brown,),
+                DropdownButton<String>(
+                  value: raza,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      raza = newValue;
+                    });
+                  },
+                  items: _razaDeMascota
+                      .map<DropdownMenuItem<String>>((String value){
+                      return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
+      
+     // abajo esta los dos iconos el de agregar la informacion al collection 
+      // y el de volver a la pantalla de mascota
+      
+      // final currentUser = await FirebaseAuth.instance.currentUser();
+      // owner = currentUser.email.toString();
+      // _addData()
   }
 }
+     
