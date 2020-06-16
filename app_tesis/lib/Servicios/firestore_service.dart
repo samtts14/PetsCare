5890b47c4dc45137a3353204d86593ce0efe63b1
samtts14 <38091875+samtts14@users.noreply.github.com>
2020-06-16 00:03:14 -0400

    actualizacion prueba de mascota

--- app_tesis/lib/Servicios/firestore_service.dart
+++ app_tesis/lib/Servicios/firestore_service.dart
@@ -1,59 +1,32 @@
 import 'package:app_tesis/Servicios/note.dart';
-// import 'package:app_tesis/models/pet.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService{
-  // static final FirestoreService _firestoreService = FirestoreService._internal();
+  static final FirestoreService _firestoreService = FirestoreService._internal();
   Firestore _db= Firestore.instance;
 
-  // FirestoreService._internal(); 
-  final String uid;
-  FirestoreService({ this.uid });
+  FirestoreService._internal(); 
 
-  final CollectionReference petsCollection = Firestore.instance.collection('pets');
-
-  Future updatePetData(String name, String species, String breed, String sex, String age, String owner) async{
-    return await petsCollection.document(uid).setData({
-      'name': name,
-      'species' : species,
-      'breed' : breed,
-      'sex' : sex,
-      'age' : age,
-      'owner': owner,
-    });
+  factory FirestoreService(){
+    return _firestoreService;
   }
-
-  // Future<void> addPet(Pet pet){
-  //   return _db.collection('pets').add(pet.toMap());
-  // }
-
-  
-  // factory FirestoreService(){
-  //   return _firestoreService;
-  // }
-  /// para manejo del uid
-
-  /// para manejo de las notas
   Stream<List<Note>>getNotas(){
     return _db
-    .collection('notes').snapshots().map(
+    .collection('note').snapshots().map(
       (snapshot)=>snapshot.documents.map(
-        (doc){
-          var documentID = doc.documentID;
-          return Note.fromMap(doc.data, documentID);
-        },
+        (doc)=>Note.fromMap(doc.data, doc.documentID),
       ).toList(),
     );
   }
   Future<void> addNote(Note note){
-    return _db.collection('notes').add(note.toMap());
+    return _db.collection('note').add(note.toMap());
   }
   Future<void> deleteNote(String id){
-    return _db.collection('notes').document(id).delete();
+    return _db.collection('note').document(id).delete();
   }
 
   Future<void> updateNote(Note note){
-    return _db.collection('notes').document(note.id).updateData(note.toMap());
+    return _db.collection('note').document(note.id).updateData(note.toMap());
   }
 
 }
